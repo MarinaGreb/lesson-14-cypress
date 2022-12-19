876ef57bb3902bc8025b405a79a40172977431e9
marinagrebneva <sokovets@outlook.com>
2022-12-19 23:05:19 +0100

    Added prod environment

--- cypress.config-2.js
+++ cypress.config-2.js
@@ -2,14 +2,14 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://staging.lpitko.ru",
+    baseUrl: "https://santa-secret.ru",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
   projectId: "xtair1",
   env: {
-    login: "sokovets@outlook.com",
-    password: "123456",
+    login: "sokovets+prod@outlook.com",
+    password: "prod123",
   },
 });
--- cypress/e2e/santa/santa_spec2.cy.js
+++ cypress/e2e/santa/santa_spec2.cy.js
@@ -1,32 +1,39 @@
-describe('Checking links on the user page after login', ()=> {
-    
-    beforeEach(() => {
-        // const username = 'sokovets@outlook.com'
-        // const password = '123456'
-        cy.visit('/login')
-        cy.get('input[name="email"]').type('sokovets@outlook.com'), 
-        cy.get('input[name="password"]').type('123456'),
-        cy.get('[class="btn-main base--clickable"]').click()
-      })
+describe("Checking links on the user page after login", () => {
+  const username = Cypress.env("login");
+  const password = Cypress.env("password");
+  beforeEach(() => {
+    cy.visit("/login");
+    cy.get('input[name="email"]').type(username);
+    cy.get('input[name="password"]').type(password);
+    cy.intercept("GET", "/api/session", []).as("authorizationPassed");
+    cy.get('[class="btn-main base--clickable"]').click();
+    //  cy.wait(4000)
+    cy.wait("@authorizationPassed");
+  });
 
-    it('new box', ()=> {
-        cy.visit('/box/new');
-        cy.url().should('include', '/box/new')
-        cy.get('[class="frm-wrapper"] div').should('have.length', 2)
-    })
-    it('view boxes', ()=> {
-        cy.visit('/account/boxes');
-        cy.url().should('include', '/account/boxes')
-        cy.get('[class="base--clickable toggle-menu-item toggle-menu-item--active"]').should('have.text', 'Мои Коробки')
-    })
-    it('randomizer', ()=> {
-        cy.visit('/randomizer');
-        cy.url().should('include', '/randomizer')
-        cy.get('[class="switch__toggle"]').should('be.visible')
-    })
-    it('account', ()=> {
-        cy.visit('/account');
-        cy.url().should('include', '/account')
-        cy.get('[class="txt--med txt txt--orange"]').should('have.text', ' Выйти с сайта')
-    })
-})
+  it("new box", () => {
+    cy.visit("/box/new");
+    cy.url().should("include", "/box/new");
+    cy.get('[class="frm-wrapper"] div').should("have.length", 2);
+  });
+  it("view boxes", () => {
+    cy.visit("/account/boxes");
+    cy.url().should("include", "/account/boxes");
+    cy.get(
+      '[class="base--clickable toggle-menu-item toggle-menu-item--active"]'
+    ).should("have.text", "Мои Коробки");
+  });
+  it("randomizer", () => {
+    cy.visit("/randomizer");
+    cy.url().should("include", "/randomizer");
+    cy.get('[class="switch__toggle"]').should("be.visible");
+  });
+  it("account", () => {
+    cy.visit("/account");
+    cy.url().should("include", "/account");
+    cy.get('[class="txt--med txt txt--orange"]').should(
+      "have.text",
+      " Выйти с сайта"
+    );
+  });
+});
